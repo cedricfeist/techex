88133308be0e84d609fb6fb1f819ae6368188a1e
Cedric Feist <cedric.feist@gmail.com>
2025-05-25 21:17:47 +0200

    no k8s

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "db_private_ip" {
   value = aws_instance.mongodb_instance.private_ip
 }
 
-output "lb_dns_endpoint" {
-  value = kubernetes_service.tasky_svc.status[0].load_balancer[0].ingress[0].hostname
-}
+#output "lb_dns_endpoint" {
+#  value = kubernetes_service.tasky_svc.status[0].load_balancer[0].ingress[0].hostname
+#}
