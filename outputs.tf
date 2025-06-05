9722cbcb4eb23f474471eede9b3338315501e6ca
Cedric Feist <cedric.feist@gmail.com>
2025-06-05 11:46:50 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,27 @@
+
+
+output "db_private_ip" {
+  value = aws_instance.mongodb_instance.private_ip
+}
+
 output "dbdnsssh" {
   value = aws_instance.mongodb_instance.public_dns
 }
 
-output "db_private_ip" {
-  value = aws_instance.mongodb_instance.private_ip
+output "db_public_ip" {
+  value = aws_instance.mongodb_instance.public_ip
+}
+
+output "attacker_private_ip" {
+  value = aws_instance.attacker.private_ip
 }
 
 output "attacker_dns" {
   value = aws_instance.attacker.public_dns
 }
 
-output "attacker_ip" {
-  value = aws_instance.attacker.private_ip
+output "attacker_public_ip" {
+  value = aws_instance.attacker.public_ip
 }
 
 output "lb_dns_endpoint" {
